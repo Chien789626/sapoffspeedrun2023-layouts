0e400b547386a828407e9a54b0a1b388ae3fc066
Chien789626 <grf8ctoc_flbo@outlook.com>
2023-05-23 11:05:43 +0900

    Does not contain build files

--- extension/example.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const nodecg_1 = require("./util/nodecg");
-(0, nodecg_1.get)().log.info('Extension code working!');
-// Access the bundle configuration with types.
-const config = (0, nodecg_1.get)().bundleConfig;
-// Accessing normal types.
-const exampleType = { exampleProperty: 'exampleString' };
